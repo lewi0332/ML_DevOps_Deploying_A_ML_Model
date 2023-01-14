3f289c9dd314f5cf73c5a72e7d98f86ad4b47839
lewi0332 <derrickjameslewis@gmail.com>
2023-01-14 14:56:01 -0600

    add dvc to heroku

--- heroku_test.py
+++ heroku_test.py
@@ -1,3 +1,12 @@
+import pandas as pd
 import requests
 import json
+
+df = pd.read_csv("data/census.csv")
+
+test = df.drop('salary', axis=1).iloc[0].to_dict()
+
+# Post request to the API.
+response = requests.post("https://ml-devops-project-3.herokuapp.com/predict", data=json.dumps(test))
+print(response.json()) # "<=50k"
 
